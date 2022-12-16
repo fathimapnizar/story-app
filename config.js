c1443b8bb973237fa91e6a979c79b0ea6d0eba9d
Fathima P Nizar <70809904+fathimapnizar@users.noreply.github.com>
2022-12-16 11:28:17 +0400

    Add files via upload

---
+++ config.js
@@ -0,0 +1,13 @@
+
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyBl6QAX5Rgn8JdN9ppZJS3yYBDBQVQoOps",
+  authDomain: "storytelling-app-55352.firebaseapp.com",
+  databaseURL: "https://storytelling-app-55352-default-rtdb.firebaseio.com",
+  projectId: "storytelling-app-55352",
+  storageBucket: "storytelling-app-55352.appspot.com",
+  messagingSenderId: "713035508714",
+  appId: "1:713035508714:web:63e70328b2d3cdb94a6093"
+};
+  // Initialize Firebase
+  //if(!firebase.app.length)
